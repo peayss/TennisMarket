11a2d6fad9dfcf7e2010384789b1622486d5d223
peayss <pys020831@naver.com>
2024-08-24 21:46:12 +0900

    shopping mall code is uploaded!!!!!

---
+++ requestHandler.js
@@ -0,0 +1,84 @@
+/*화면 바꾸는 방법*/
+const fs = require('fs');
+const mariadb = require('./database/connect/mariadb');
+const orderlist_view = fs.readFileSync('/orderlist.html');
+const main_view = fs.readFileSync('./main.html');
+
+function main(response){
+    console.log('main');
+
+    mariadb.query("SELECT * FROM product", function(err,rows){
+        console.log(rows);
+    });
+
+    response.writeHead(200,{'Content-Type' : 'text/html'});
+    response.write(main_view);
+    response.end();
+}
+
+function redRacket(response){
+    fs.readFile('./img/redRacket.png', function(err,data){
+        response.writeHead(200,{'Content-Type' : 'text/html'});
+        response.write(data);
+        response.end();
+    })
+}
+function blueRacket(response){
+    fs.readFile('./img/blueRacket.png', function(err,data){
+        response.writeHead(200,{'Content-Type' : 'text/html'});
+        response.write(data);
+        response.end();
+    })
+}
+function blackRacket(response){
+    fs.readFile('./img/blackRacket.png', function(err,data){
+        response.writeHead(200,{'Content-Type' : 'text/html'});
+        response.write(data);
+        response.end();
+    })
+}
+function mainCss(response){
+    fs.readFile('./tennis.css', function(err,data){
+        response.writeHead(200,{'Content-Type' : 'text/css'});
+        response.write(data);
+        response.end();
+    })
+}
+function order(response, productId){
+    response.writeHead(200,{'Content-Type' : 'text/html'});
+
+    mariadb.query("INSERT INTO orderlist VALUES(" + productId + ", '" + new Date().toLocaleDateString() + "');", function(err,rows){
+        console.log(rows);
+    })
+    response.write('order page');
+    response.end();
+}
+function orderlist(response){
+    response.writeHead(200,{'Content-Type' : 'text/html'});
+
+    mariadb.query("SELECT * FROM orderlist", function(err,rows){
+        response.write(orderlist_view);
+
+        rows.forEach(element => {
+            response.write("<tr>"
+                + "<td>" + element.productId+"</td>"
+                + "<td>" + element.order_date+"</td>"
+                + "</tr>");
+        });
+        response.write("</table>");
+        response.end();
+    })
+}
+
+let handle = {}; // key:value 
+handle['/'] = main;
+handle['/order'] = order;
+handle['/orderlist'] = orderlist;
+
+/* image directory */
+handle['/img/redRacket.png'] = redRacket;
+handle['/img/blueRacket.png'] = blueRacket;
+handle['/img/blackRacket.png'] = blackRacket;
+
+handle['/tennis.css'] = mainCss;
+exports.handle = handle;
